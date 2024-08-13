fc5287f54c89c73358960e50b5666d8daf74c352
Pedro Henrique Queiroz Miranda <pmiranda7410@gmail.com>
2024-08-13 20:01:04 -0300

    Feature Cadastro e Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  self.inheritance_column = :_type_disabled
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "user#dashboard"
-
-  # get '/show', to: 'articles#show'
-  get '/login', to: 'session#new'
-  post '/session/create', to: 'session#create'
-
   # MÉTODOS QUE EXISTEM PARA ROTAS:
   # GET: Para receber algum dado. Um template ou um dado.
-  # POST, UPDATE, DELETE: Executar alguma ação
+  # POST, UPDATE (patch), DELETE: Executar alguma ação
+
+  # resources :users
+  # resources :sessions
+  # resources :lessons
+  # resources :comments
+  root "user#dashboard"
+
+  get '/login', to: 'session#new'
+
+  # CRUD - Users
+  get '/user/new', to: 'user#new'
+  get '/user/:id/edit', to: 'user#edit'
+  post '/user/create', to: 'user#create'
+  patch '/user/:id/update', to: 'user#update'
+  delete '/user/:id/delete', to: 'user#delete'
+
+  post '/session/create', to: 'session#create'
 end
